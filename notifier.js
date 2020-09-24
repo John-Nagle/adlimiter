6fd040f1ddb6820e460e4045cc0a3092c48aa123
John-Nagle <nagle@sitetruth.com>
2020-09-23 19:25:08 -0700

    Begin 3.2.0, with opt-in dialog

--- notifier.js
+++ notifier.js
@@ -27,4 +27,25 @@ function notify(message) {
 }
 //
 browser.runtime.onMessage.addListener(notify);                          // connect fn to event
+
+//  
+//  Opt-in interface. Brings up a popup.
+//
+browser.runtime.onMessage.addListener(function(message, sender) {
+  // check storage for opt in
+  browser.storage.local.get("optIn", function(result) {
+    // send message back to content script with value of opt in
+    browser.tabs.sendMessage(
+      sender.tab.id, { "optIn" : (true == result.optIn)});
+  });
+});
+
+// show the tab if we haven't registered the user reacting to the prompt.
+browser.storage.local.get("optInShown", function(result) {
+  if (!result.optInShown) {
+    browser.tabs.create({ url: "popup/opt-in.html" });
+  }
+});
+
+
 console.log("AdLimiter background loaded.");                            // ***TEMP***
