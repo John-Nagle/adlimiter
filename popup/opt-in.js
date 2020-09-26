2ebd869c38c3247ca3d739922999c02cb462fbc7
John-Nagle <nagle@sitetruth.com>
2020-09-25 22:49:12 -0700

    Uninstall if user rejects opt-in

--- popup/opt-in.js
+++ popup/opt-in.js
@@ -18,7 +18,12 @@ window.addEventListener("load", function() {
   document.getElementById("button-cancel").addEventListener(
     "click",
     function() {
-      browser.storage.local.set({ "optIn" : false, "optInShown" : true });
-      window.close();
+      browser.storage.local.set({ "optIn" : false, "optInShown" : false });         // either we get removed or have to do this again
+      //    If the user confirms the remove, the add-on is gone, and there's nothing more to do.
+        function removed() {window.close(); console.log("Ad Limiter removed.");}                                         // add-on was removed, nothing to do
+        function kept() {}                                             // kept, will ask again
+      //    If the user cancels the remove, the opt-in prompt will be shown again.
+      browser.management.uninstallSelf({"showConfirmDialog":true, "dialogMessage": "Remove Ad Limiter add-on?"}).then(removed,kept);
+      ////window.close();
   });
 });
