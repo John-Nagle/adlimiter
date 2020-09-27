32426c63bde19b3e7d0b9f653c9d807a482899f2
John-Nagle <nagle@sitetruth.com>
2020-09-26 19:43:16 -0700

    Works under Chrome but will not uninstall

--- popup/opt-in.js
+++ popup/opt-in.js
@@ -2,11 +2,13 @@
 
 window.addEventListener("load", function() {
   // set up the appearance of the popup depending on the outcome of the opt-in
+  /*
   browser.storage.local.get("optInShown", function(result) {
     console.log("Setting up UI. result.optInShown:" + result.optInShown);
     document.getElementById("opt-in-prompt").hidden = result.optInShown;
     document.getElementById("after-opt-in").hidden = !result.optInShown;
   });
+  */
 
   document.getElementById("button-enable").addEventListener(
     "click",
