8cff3dba8295f16f0b9f121e3cce0f52513a77f1
John-Nagle <nagle@sitetruth.com>
2020-09-28 13:20:30 -0700

    Appears to work with opt-in dialog

--- popup/opt-in.js
+++ popup/opt-in.js
@@ -4,9 +4,8 @@ window.addEventListener("load", function() {
 
     // Set up the appearance of the pop-up depending upon
     // whether this is a new install or an upgrade
-    console.log("Loaded popup");// ***TEMP***
-    browser.storage.local.get("T")                                  // were there old cache entries?
-    .then(function(result) { console.log("Got T: %o",result); document.getElementById("upgrade-info").hidden = !("T" in result); });   // upgrade if T present
+    browser.storage.local.get("T")                                  // were there old cache entries? If yes, this is an upgrade.
+    .then(function(result) { document.getElementById("upgrade-info").hidden = !("T" in result); });   // upgrade if T present
 
     document.getElementById("button-enable").addEventListener(
     "click",
